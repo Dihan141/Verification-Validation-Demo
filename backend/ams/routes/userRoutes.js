b645820e2c9c3e524dcaeaa35b193386bd21771a
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2025-06-27 00:09:28 +0600

    new routes to add user and fetch user by id

--- backend/ams/routes/userRoutes.js
+++ backend/ams/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getUsers } = require('../controller/userController');
+const { getUsers, addUser, getUserById } = require('../controller/userController');
 const router = express.Router();
 
 router.get('/', getUsers);
+router.post('/add', addUser);
+router.get('/:userId', getUserById);
 
 module.exports = router;
